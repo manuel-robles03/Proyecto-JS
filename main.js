8e19405860560f6faa6390ec99b12bffe9dd6519
Manuel Robles <manuelrobles3304@gmail.com>
2023-08-15 22:38:28 -0300

    terminando para entregar

--- main.js
+++ main.js
@@ -106,8 +106,8 @@ const detalles = document.querySelector(`.Alerta`)
 comprar.addEventListener("click", () => {
     const confirmacion = document.createElement(`div`)
     confirmacion.classList.add("Alerta")
-    confirmacion.innerText = 
+    confirmacion.innerHTML = 
     
-    `<h2> Felicidades, has realizado una compra. DETALLES DE COMPRA: - Cantidad de camisetas compradas: ` + stock.value + ` - Talle : ` + talle.value + ` - Modelo : ` + modelo.value ` </h2>`
+    `<h2> Felicidades, has realizado una compra. DETALLES DE COMPRA: - Cantidad de camisetas compradas: ` + stock.value + ` - Talle : ` + talle.value + ` - Modelo : ` + modelo.value + ` </h2>`
     detalles.appendChild(confirmacion)
 })
